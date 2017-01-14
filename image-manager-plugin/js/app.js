da84367188da1da218814b700098fba572cd7faf
karszczak <daniel.karski@gmail.com>
2017-01-14 18:19:07 +0100

    change init plugin

--- image-manager-plugin/js/app.js
+++ image-manager-plugin/js/app.js
@@ -3,6 +3,10 @@
         this.container = new window.app.Container();
     }
 
+    ImageManager.prototype.init = function() {
+        this.container.setContainer();
+    };
+
     // Export to window
     window.app = window.app || {};
     window.app.ImageManager = ImageManager;
