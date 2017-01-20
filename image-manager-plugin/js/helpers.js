887f77cec345122c4aa9f8fdaf4266e0731ff400
karszczak <daniel.karski@gmail.com>
2017-01-20 09:44:12 +0100

    init project with another concept

--- image-manager-plugin/js/helpers.js
+++ image-manager-plugin/js/helpers.js
@@ -1,23 +1,9 @@
+/**
+ * Created by b50 on 2017-01-20.
+ */
+/*global NodeList */
 (function (window) {
-
-    // Get element(s) by CSS selector:
-    window.qs = function (selector, scope) {
-        return (scope || document).querySelector(selector);
-    };
-    window.qsa = function (selector, scope) {
-        return (scope || document).querySelectorAll(selector);
-    };
-
-    // addEventListener wrapper:
     window.$on = function (target, type, callback, useCapture) {
         target.addEventListener(type, callback, !!useCapture);
     };
-
-    //create element
-    window.elt = function(name, className) {
-        var elt = document.createElement(name);
-        if (className) elt.className = className;
-        return elt;
-    }
-
 })(window);
