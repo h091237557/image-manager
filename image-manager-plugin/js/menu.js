a337bb4f3b603f3af2eaf0f7548a7b02235460c8
karszczak <daniel.karski@gmail.com>
2017-01-20 09:31:14 +0100

    add edit panel and content to menu

--- image-manager-plugin/js/menu.js
+++ image-manager-plugin/js/menu.js
@@ -3,9 +3,23 @@
         var instance;
 
         function init(){
-            var container = window.elt('div', 'image-manager__menu'),
+            var container = window.elt('div', 'menu'),
+                content = window.elt('div', 'menu__content'),
+                editPanel = window.elt('div', 'menu__edit-panel'),
+                deleteAllButton = window.elt('button', 'deleteAll'),
+                checkAllButton = window.elt('button', 'checkAll'),
                 imageList = window.app.ImageList.getInstance();
 
+            container.appendChild(content);
+            container.appendChild(editPanel);
+
+            editPanel.appendChild(deleteAllButton);
+            editPanel.appendChild(checkAllButton);
+
+            checkAllButton.innerHTML = 'Check all';
+            deleteAllButton.innerHTML = 'Delete all';
+
+            deleteAllButton.addEventListener('click', deleteAll);
             actualView();
 
             function checkAll(){
@@ -13,11 +27,12 @@
             }
 
             function deleteAll(){
-
+                imageList.invoke('delete');
             }
 
             function actualView(){
-                container.innerHTML = imageList.count();
+                var plural = imageList.count() === 1 ? '' : 's';
+                content.innerHTML = imageList.count() + ' image' + plural;
             }
 
             return {
